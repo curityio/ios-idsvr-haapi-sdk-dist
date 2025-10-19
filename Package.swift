514dc99546ed635e9574833b52933e66a48e0bd2
Henri La <henri.la@curity.io>
2025-10-19 21:50:41 +0200

    HSI-376 updates swiftLanguageModes to .v6 and swift-tools-version to 6.2

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version: 6.2
 
 import PackageDescription
 
@@ -20,5 +20,5 @@ let package = Package(
             path: "IdsvrHaapiSdk.xcframework"
         )
     ],
-    swiftLanguageVersions: [.v5]
+    swiftLanguageModes: [.v6]
 )
